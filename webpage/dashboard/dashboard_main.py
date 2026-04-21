203228cca16e221879c7b576687bb52cb33860a2
Marco Michaelis <michaelis.marco@gmx.de>
2026-04-21 15:56:59 +0200

    insert tables and status traffic light in page 'Packaging Line Matrix'

--- webpage/dashboard/dashboard_main.py
+++ webpage/dashboard/dashboard_main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
 from dashboard.kpi_chart_calculations import get_data
 from dashboard.kpi_chart_calculations import kpi_speedconveyor
